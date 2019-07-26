1aceacd80b6d4647e50dd5758fcf77319ee78b3d
SQUILLACI Samuel <45792776+ssquilla@users.noreply.github.com>
2019-07-26 17:48:58 +0100

    Add files via upload

--- battery_git_sam/mainBattery2.py
+++ battery_git_sam/mainBattery2.py
@@ -30,6 +30,7 @@ from time import time
 from copy import deepcopy
 from functools import reduce
 from random import shuffle
+from random import randint
 
 def saveObject(obj,filename):
     outfile = open(filename,'wb')
@@ -41,22 +42,6 @@ def loadObject(filename):
     obj = pk.load(infile,encoding='latin1')
     infile.close()
     return obj
-
-"""
-Notes about my libs:
-    Data Manager is a top layer embedding the dataset to add operations on it.
-    Profile miner use categorical data to exract a tree of profiles
-
-
-Few observations : 
-    data types : why keep measures as int and not float? I used float to detect
-        data nature (categorical vs continuous) so I converted some column into
-        float types.
-        Id don't interested me in the first place.
-        However it's not difficult at all to change this.
-        Categorical data in the files are all string (object in pandas).
-        So I need to collect the int values to cast all of the with "astype"
-"""
 
 savePicklePath = "save/pickle_objects/"
 
@@ -80,3 +65,90 @@ sh0 = d0.dataset.shape
 nan_counts0 = d0.dataset.isna().sum()
 # see plot in the vizu folder as pairplot.png
 #sns.pairplot(d0.dataset)
+
+
+d0.delColumns(["bmselectricalmodulemonitoring_hvbattfusetemperature",
+ "messageheader_messagebodytype",
+				"thermalmanagement_hvbattcoolingenergyusd"])
+
+print(d0.typesSummary())
+intToFloat = ['other_odometermastervalue',
+       'other_recordedtime_seconds',
+       'batterycellmeasurements_hvbattvoltageext',
+       'bmselectricalmodulemonitoring_hvbattmemtemperature',
+       'thermalmanagement_hvbattinletcoolanttemp',
+       'thermalmanagement_hvbattoutletcoolanttemp']
+d0.dataset[intToFloat] = d0.dataset[intToFloat].astype("float64")
+stringToDate = ['_actualtime',
+       'produceddatetime_WirelessCar',
+       'messageheader_timestamp']
+for date_att in stringToDate:
+    d0.dataset[date_att] = pd.to_datetime(d0.dataset[date_att])
+d0.setInfos() # my lib needs to refresh information when types change    
+print(d0.typesSummary())
+
+# chose some columns to study
+cols_select = ['encryptedvin', 'analyticvin','_cycle','_num_cycle', # groupby cols
+               # some measurments
+               'IMBALANCE_DELTA_actual',
+               'batterycellmeasurements_hvbattcurrentext',
+               'batteryhealthmonitoring_hvbattstateofhealthpwr',
+               'batteryhealthmonitoring_hvbatstateofhealthmax',
+               'batterycellmeasurements_hvbattcelltemphottest',
+               'bmselectricalmodulemonitoring_hvbattfusetemperature',
+               'bmselectricalmodulemonitoring_hvbattmemtemperature',
+               # car mode
+               'other_powermode', 'other_odometermastervalue',
+               'other_ambienttemp', 'other_vehiclespeed',
+               'other_recordedtime_seconds','cellbalancing_hvbattbalancingstatus',
+               # errors
+               'batteryfaultmonitoring_hvbatstatucat4derate',
+               'batteryfaultmonitoring_hvbatstatuscat6dlybpo',
+               'batteryfaultmonitoring_hvbatstatuscat7nowbpo']
+d0.select(cols_select)
+vin_groups = d0.dataset.groupby(by=['encryptedvin', 'analyticvin','_cycle',
+               '_num_cycle'])
+
+"""
+filtered = vin_groups.filter(lambda x: x["IMBALANCE_Percent"].max() > 0.05 and x.shape[0]>5)
+filtered = filtered.reset_index()
+
+regroup = filtered.groupby(by=['encryptedvin', 'analyticvin','_cycle',
+               '_num_cycle'])
+"""
+
+n_groups = len(list(vin_groups.groups.keys()))
+idxGroup = randint(0,n_groups)
+key = list(vin_groups.groups.keys())[idxGroup]
+n_points = 10
+group = d0.dataset.iloc[vin_groups.groups[key]][0:n_points]
+
+plt.figure()
+plt.title(str(key))
+X = group['other_recordedtime_seconds']
+plt.plot(X,1000*np.array(group['IMBALANCE_DELTA_actual']),color="r")
+plt.plot(X,group['batterycellmeasurements_hvbattcurrentext'],color="y")
+plt.plot(X,group['batteryhealthmonitoring_hvbattstateofhealthpwr'],color="g")
+plt.plot(X,group['batteryhealthmonitoring_hvbatstateofhealthmax'],color="b")
+plt.plot(X,group['batterycellmeasurements_hvbattcelltemphottest'],color="c")
+plt.plot(X,group['bmselectricalmodulemonitoring_hvbattfusetemperature'],color="m")
+plt.plot(X,group['bmselectricalmodulemonitoring_hvbattmemtemperature'],color="r")
+plt.plot(X,[0]*len(X),'--k')
+plt.plot(X,[30]*len(X),'--k')
+plt.plot(X,[50]*len(X),'--k')
+plt.legend(['1000*IMBALANCE_Percent',
+               'batterycellmeasurements_hvbattcurrentext',
+               'batteryhealthmonitoring_hvbattstateofhealthpwr',
+               'batteryhealthmonitoring_hvbatstateofhealthmax',
+               'batterycellmeasurements_hvbattcelltemphottest',
+               'bmselectricalmodulemonitoring_hvbattfusetemperature',
+               'bmselectricalmodulemonitoring_hvbattmemtemperature'])
+plt.xlabel("record time (s)")
+plt.ylabel("measure")
+plt.show()
+
+
+
+
+
+
